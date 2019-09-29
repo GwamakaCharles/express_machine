fe9bcfbdfa3b7601b66a5e326227db3fcfab9b8a
Gwamaka Charles <gwaxart19@gmail.com>
2019-09-29 19:25:37 +0300

    Add the ability to retrieve all animals and a single animal.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 const { getElementById, getIndexById, updateElement,
-        seedElements, createElement } = require('./utils');
+  seedElements, createElement } = require('./utils');
 
 const expressions = [];
 seedElements(expressions, 'expressions');
@@ -24,6 +24,19 @@ app.get('/expressions/:id', (req, res, next) => {
   const foundExpression = getElementById(req.params.id, expressions);
   if (foundExpression) {
     res.send(foundExpression);
+  } else {
+    res.status(404).send();
+  }
+});
+
+app.get('/animals', (req, res, next) => {
+  res.send(animals);
+});
+
+app.get('/animals/:id', (req, res, next) => {
+  const foundAnimal = getElementById(req.params.id, animals);
+  if (foundAnimal) {
+    res.send(foundAnimal);
   } else {
     res.status(404).send();
   }
@@ -60,5 +73,5 @@ app.delete('/expressions/:id', (req, res, next) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`); 
+  console.log(`Listening on port ${PORT}`);
 });
